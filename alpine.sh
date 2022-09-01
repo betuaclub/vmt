844b0f15c9a7f61fafb22c64d119d86d9b1b2f1a
betuaclub <98859816+betuaclub@users.noreply.github.com>
2022-09-01 14:59:25 +0300

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -15,6 +15,5 @@ echo -n  "This is one more line"
 curl http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/x86_64/alpine-virt-3.10.1-x86_64.iso -o alpine.iso
 echo -n  "This is one more line"
 qemu-img create -f qcow2 alpine.img 5g
-echo -n  "This is one more line"
- qemu-system-x86-64 -hda alpine.img -cdrom alpine.iso -boot d -m 512
-
+echo -n  "This is one more "
+qemu-system-x86_64 -hda alpine.img -cdrom alpine.iso -boot d -m 512
