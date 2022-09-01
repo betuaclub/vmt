c8c3331213475ca38838644ca0edded0987ec4fa
betuaclub <98859816+betuaclub@users.noreply.github.com>
2022-09-01 14:02:43 +0300

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -1,6 +1,9 @@
 pkg install x11-repo
 pkg install unstable-repo
-pkg install qemu-system-x64_64 qemu-utils
+pkg install unstable-repo
+pkg install qemu-system-x86_64-headless
+pkg install x11-repo
+pkg install qemu-system-x86_64
 pkg install curl
 curl http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/x86_64/alpine-virt-3.10.1-x86_64.iso -o alpine.iso
 qemu-img create -f qcow2 alpine.img 5g
