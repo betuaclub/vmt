5d30806e29d43d81d06093b6d71267078fc77667
betuaclub <98859816+betuaclub@users.noreply.github.com>
2022-09-01 14:44:59 +0300

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -8,7 +8,7 @@ pkg install unstable-repo
 echo -n  "This is one more line"
 pkg install qemu-system-x86_64-headless
 echo -e "\033[1;31mNow we are good with usage of echo command\033[0m"
-pkg install qemu-system-x86_64
+pkg install qemu-system-x86-64
 echo -n  "This is one more line"
 pkg install curl
 echo -n  "This is one more line"
@@ -16,5 +16,5 @@ curl http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/x86_64/alpine-virt-3.10
 echo -n  "This is one more line"
 qemu-img create -f qcow2 alpine.img 5g
 echo -n  "This is one more line"
-qemu-system-x86_64 -hda alpine.img -cdrom alpine.iso -boot d -m 512
+ qemu-system-x86-64 -hda alpine.img -cdrom alpine.iso -boot d -m 512
 
